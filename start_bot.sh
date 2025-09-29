cfb230ad5536b6a2e3831cf7051c7c82b51b5f35
Rochana99 <116473568+Rochana99@users.noreply.github.com>
2025-09-29 16:27:11 +0530

    Create start_bot.sh

---
+++ start_bot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Bot එක ආරම්භ කරන්න, එහි output Console එකටම print වේ.
+# මෙහිදී process එක background එකට යවන්නේ නැත, එබැවින් Pair Code එක කෙලින්ම console එකට print වේ.
+# මෙම script එක GitHub Actions Step එක අවසන් වන තුරු "Block" කර තබා ගනී.
+
+node index.js
